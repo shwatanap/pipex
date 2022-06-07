41c21428c7fd24060f466f996a6651c8faa6eeef
shwatanap <watanabeshou1520@gmail.com>
2022-06-08 06:44:33 +0900

    :memo: #1 processによってfdを変更

--- srcs/pipex_bonus.c
+++ srcs/pipex_bonus.c
@@ -6,54 +6,65 @@
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 03:40:55 by shwatana          #+#    #+#             */
-/*   Updated: 2022/06/04 23:10:12 by shwatana         ###   ########.fr       */
+/*   Updated: 2022/06/08 06:43:04 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex_bonus.h"
 
-static int	input_process(int argc, char **argv, int *out_file_fd);
+static int	input_process(int argc, char **argv, int (*file_fd)[2]);
 static void	here_doc(char *limiter);
 static void	here_doc_child_process(int pipe_fd[2], char *limiter);
-static void	child_process(char *cmd, char **envp);
+static void	child_process(char *cmd, char **envp, t_process_mode process_mode,
+				int file_fd[2]);
 
 int	main(int argc, char **argv, char **envp)
 {
-	int	arg_idx;
-	int	out_file_fd;
+	int				arg_idx;
+	int				file_fd[2];
+	t_process_mode	process_mode;
 
-	if (argc >= 5)
+	if (argc < 5)
+		display_usage_with_exit();
+	arg_idx = input_process(argc, argv, &file_fd);
+	while (arg_idx < argc - 1)
 	{
-		arg_idx = input_process(argc, argv, &out_file_fd);
-		while (arg_idx < argc - 2)
-			child_process(argv[arg_idx++], envp);
-		close(STDOUT_FILENO);
-		dup2(out_file_fd, STDOUT_FILENO);
-		close(out_file_fd);
-		execute(argv[argc - 2], envp);
+		// TODO: heredocにも対応させる
+		if (arg_idx == 2)
+			process_mode = FIRST_PROCESS;
+		else if (arg_idx == argc - 2)
+			process_mode = LAST_PROCESS;
+		else
+			process_mode = NORMAL_PROCESS;
+		child_process(argv[arg_idx], envp, process_mode, file_fd);
+		arg_idx++;
 	}
-	display_usage_with_exit();
+	// close(STDOUT_FILENO);
+	// dup2(out_file_fd, STDOUT_FILENO);
+	// close(out_file_fd);
+	// execute(argv[argc - 2], envp);
+	wait(NULL);
 }
 
-static int	input_process(int argc, char **argv, int *out_file_fd)
+static int	input_process(int argc, char **argv, int (*file_fd)[2])
 {
-	int	in_file_fd;
 	int	first_cmd_idx;
 
 	if (ft_strncmp(argv[1], HERE_DOG_STR, HERE_DOG_STR_LEN) == SUCCESS)
 	{
 		if (argc < 6)
 			display_usage_with_exit();
-		*out_file_fd = open_file(argv[argc - 1], FILE_APPEND_WRITE);
+		(*file_fd)[1] = open_file(argv[argc - 1], FILE_APPEND_WRITE);
+		// TODO: here_docのinfile対応
 		here_doc(argv[2]);
 		first_cmd_idx = 3;
 		return (first_cmd_idx);
 	}
-	*out_file_fd = open_file(argv[argc - 1], FILE_OVER_WRITE);
-	in_file_fd = open_file(argv[1], FILE_READ);
-	close(STDIN_FILENO);
-	dup2(in_file_fd, STDIN_FILENO);
-	close(in_file_fd);
+	(*file_fd)[1] = open_file(argv[argc - 1], FILE_OVER_WRITE);
+	(*file_fd)[0] = open_file(argv[1], FILE_READ);
+	// close(STDIN_FILENO);
+	// dup2(in_file_fd, STDIN_FILENO);
+	// close(in_file_fd);
 	first_cmd_idx = 2;
 	return (first_cmd_idx);
 }
@@ -95,7 +106,8 @@ static void	here_doc_child_process(int pipe_fd[2], char *limiter)
 	exit(EXIT_FAILURE);
 }
 
-static void	child_process(char *cmd, char **envp)
+static void	child_process(char *cmd, char **envp, t_process_mode process_mode,
+		int file_fd[2])
 {
 	int		pipe_fd[2];
 	pid_t	pid;
@@ -107,16 +119,23 @@ static void	child_process(char *cmd, char **envp)
 		perror_with_exit("fork");
 	if (pid == CPID)
 	{
-		close(pipe_fd[PIPE_IN_FD]);
-		close(STDOUT_FILENO);
-		dup2(pipe_fd[PIPE_OUT_FD], STDOUT_FILENO);
+		// close(pipe_fd[PIPE_IN_FD]);
+		// close(STDOUT_FILENO);
+		if (process_mode == FIRST_PROCESS)
+			dup2(pipe_fd[0], STDIN_FILENO);
+		else
+			dup2(pipe_fd[PIPE_IN_FD], STDIN_FILENO);
+		if (process_mode == LAST_PROCESS)
+			dup2(file_fd[1], STDOUT_FILENO);
+		else
+			dup2(pipe_fd[PIPE_OUT_FD], STDOUT_FILENO);
 		close(pipe_fd[PIPE_OUT_FD]);
 		execute(cmd, envp);
 	}
-	if (waitpid(pid, NULL, 0) == FAIL)
-		perror_with_exit("waitpid");
-	close(pipe_fd[PIPE_OUT_FD]);
-	close(STDIN_FILENO);
-	dup2(pipe_fd[PIPE_IN_FD], STDIN_FILENO);
-	close(pipe_fd[PIPE_IN_FD]);
+	// if (waitpid(pid, NULL, 0) == FAIL)
+	// 	perror_with_exit("waitpid");
+	// close(pipe_fd[PIPE_OUT_FD]);
+	// close(STDIN_FILENO);
+	// dup2(pipe_fd[PIPE_IN_FD], STDIN_FILENO);
+	// close(pipe_fd[PIPE_IN_FD]);
 }
