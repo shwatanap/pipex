97744d70775f5f3bfd4324041ecf66ddccdcec51
shwatanap <watanabeshou1520@gmail.com>
2022-06-08 14:50:04 +0900

    :memo: #1 とりあえず動く

--- srcs/pipex_bonus.c
+++ srcs/pipex_bonus.c
@@ -6,7 +6,7 @@
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 03:40:55 by shwatana          #+#    #+#             */
-/*   Updated: 2022/06/08 07:53:43 by shwatana         ###   ########.fr       */
+/*   Updated: 2022/06/08 14:48:07 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,35 +15,70 @@
 static int	input_process(int argc, char **argv, int (*file_fd)[2]);
 static void	here_doc(char *limiter);
 static void	here_doc_child_process(int pipe_fd[2], char *limiter);
-static void	child_process(char *cmd, char **envp, t_process_mode process_mode,
-				int file_fd[2]);
+static void	child_process(char *cmd, char **envp, int in_fd, int out_fd);
 
 int	main(int argc, char **argv, char **envp)
 {
-	int				arg_idx;
-	int				file_fd[2];
-	t_process_mode	process_mode;
+	int	arg_idx;
+	int	file_fd[2];
+	int	pipe_fd[2];
+	int	old_fd[2];
+	int	use_fd[2];
 
 	if (argc < 5)
 		display_usage_with_exit();
 	arg_idx = input_process(argc, argv, &file_fd);
 	while (arg_idx < argc - 1)
 	{
+		// dprintf(2, "|[1]: %d, %d|\n", file_fd[0], file_fd[1]);
+		if (arg_idx != 2)
+		{
+			close(use_fd[0]);
+			close(use_fd[1]);
+		}
+		old_fd[0] = pipe_fd[0];
+		old_fd[1] = pipe_fd[1];
 		// TODO: heredocにも対応させる
+		if (pipe(pipe_fd) == FAIL)
+			perror_with_exit("pipe");
 		if (arg_idx == 2)
-			process_mode = FIRST_PROCESS;
+		{
+			use_fd[0] = file_fd[0];
+			use_fd[1] = pipe_fd[1];
+		}
 		else if (arg_idx == argc - 2)
-			process_mode = LAST_PROCESS;
+		{
+			use_fd[0] = old_fd[0];
+			use_fd[1] = file_fd[1];
+		}
 		else
-			process_mode = NORMAL_PROCESS;
-		child_process(argv[arg_idx], envp, process_mode, file_fd);
-		dprintf(2, "idx: %d\n", arg_idx);
+		{
+			use_fd[0] = old_fd[0];
+			use_fd[1] = pipe_fd[1];
+		}
+		// dprintf(2, "|[1]: %d, %d, [2]:%d, %d|\n", use_fd[0], use_fd[1],
+		// 		pipe_fd[0], pipe_fd[1]);
+		// if (arg_idx == 2)
+		// 	child_process(argv[arg_idx], envp, file_fd[0], pipe_fd[1]);
+		// else if (arg_idx == argc - 2)
+		// 	child_process(argv[arg_idx], envp, old_fd[0], file_fd[1]);
+		// else
+		child_process(argv[arg_idx], envp, use_fd[0], use_fd[1]);
+		close(use_fd[0]);
+		close(use_fd[1]);
+		// dprintf(2, "idx: %d\n", arg_idx);
 		arg_idx++;
 	}
 	// close(STDOUT_FILENO);
 	// dup2(out_file_fd, STDOUT_FILENO);
 	// close(out_file_fd);
 	// execute(argv[argc - 2], envp);
+	close(old_fd[0]);
+	close(old_fd[1]);
+	close(pipe_fd[0]);
+	close(pipe_fd[1]);
+	close(use_fd[0]);
+	close(use_fd[1]);
 	close(file_fd[0]);
 	close(file_fd[1]);
 	while (wait(NULL) != FAIL)
@@ -110,16 +145,14 @@ static void	here_doc_child_process(int pipe_fd[2], char *limiter)
 	exit(EXIT_FAILURE);
 }
 
-static void	child_process(char *cmd, char **envp, t_process_mode process_mode,
-		int file_fd[2])
+static void	child_process(char *cmd, char **envp, int in_fd, int out_fd)
 {
-	int		pipe_fd[2];
 	pid_t	pid;
 
-	close(5);
-	close(6);
-	if (pipe(pipe_fd) == FAIL)
-		perror_with_exit("pipe");
+	// close(5);
+	// close(6);
+	// if (pipe(pipe_fd) == FAIL)
+	// 	perror_with_exit("pipe");
 	pid = fork();
 	if (pid == FAIL)
 		perror_with_exit("fork");
@@ -129,15 +162,11 @@ static void	child_process(char *cmd, char **envp, t_process_mode process_mode,
 		// close(pipe_fd[PIPE_OUT_FD]);
 		close(STDOUT_FILENO);
 		close(STDIN_FILENO);
-		dprintf(2, "|%d, %d|\n", pipe_fd[0], pipe_fd[1]);
-		if (process_mode == FIRST_PROCESS)
-			dup2(file_fd[0], STDIN_FILENO);
-		else
-			dup2(pipe_fd[PIPE_IN_FD], STDIN_FILENO);
-		if (process_mode == LAST_PROCESS)
-			dup2(file_fd[1], STDOUT_FILENO);
-		else
-			dup2(pipe_fd[PIPE_OUT_FD], STDOUT_FILENO);
+		// dprintf(2, "|%d, %d|\n", in_fd, out_fd);
+		dup2(in_fd, STDIN_FILENO);
+		dup2(out_fd, STDOUT_FILENO);
+		close(in_fd);
+		close(out_fd);
 		// close(pipe_fd[PIPE_IN_FD]);
 		// close(pipe_fd[PIPE_OUT_FD]);
 		// close(file_fd[PIPE_IN_FD]);
@@ -148,6 +177,6 @@ static void	child_process(char *cmd, char **envp, t_process_mode process_mode,
 	// close(STDIN_FILENO);
 	// dup2(pipe_fd[PIPE_IN_FD], STDIN_FILENO);
 	// close(pipe_fd[PIPE_IN_FD]);
-	wait(NULL);
-	dprintf(2, "cmd: %s\n", cmd);
+	// wait(NULL);
+	// dprintf(2, "cmd: %s\n", cmd);
 }
