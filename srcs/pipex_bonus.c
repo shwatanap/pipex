5fc8bfd801c5e2d01dfd02408efe4c96d3c71f4c
shwatanap <watanabeshou1520@gmail.com>
2022-06-09 13:02:08 +0900

    :memo: #1 update fine tuning

--- srcs/pipex_bonus.c
+++ srcs/pipex_bonus.c
@@ -6,14 +6,15 @@
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 03:40:55 by shwatana          #+#    #+#             */
-/*   Updated: 2022/06/09 12:39:41 by shwatana         ###   ########.fr       */
+/*   Updated: 2022/06/09 13:00:01 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex_bonus.h"
 
 static int	input_process(int argc, char **argv, int (*file_fd)[2]);
-static void	exec_process(t_main_arg *main_arg, int arg_idx, int file_fd[2]);
+static void	exec_process(t_main_arg *main_arg, int arg_idx, int file_fd[2],
+				bool is_first_process);
 static void	child_process(char *cmd, char **envp, int *use_fd, int pipe_in_fd);
 
 void	set_main_arg_struct(int argc, char **argv, char **envp,
@@ -28,15 +29,18 @@ int	main(int argc, char **argv, char **envp)
 {
 	int			arg_idx;
 	int			file_fd[2];
+	bool		is_first_process;
 	t_main_arg	main_arg;
 
 	set_main_arg_struct(argc, argv, envp, &main_arg);
 	if (argc < 5)
 		display_usage_with_exit();
 	arg_idx = input_process(argc, argv, &file_fd);
+	is_first_process = true;
 	while (arg_idx < argc - 1)
 	{
-		exec_process(&main_arg, arg_idx, file_fd);
+		exec_process(&main_arg, arg_idx, file_fd, is_first_process);
+		is_first_process = false;
 		arg_idx++;
 	}
 	close(file_fd[0]);
@@ -65,12 +69,13 @@ static int	input_process(int argc, char **argv, int (*file_fd)[2])
 	return (first_cmd_idx);
 }
 
-static void	exec_process(t_main_arg *main_arg, int arg_idx, int file_fd[2])
+static void	exec_process(t_main_arg *main_arg, int arg_idx, int file_fd[2],
+		bool is_first_process)
 {
 	int	use_fd[2];
 	int	pipe_fd[2];
 
-	if (arg_idx != 2)
+	if (!is_first_process)
 	{
 		close(use_fd[0]);
 		close(use_fd[1]);
@@ -80,7 +85,7 @@ static void	exec_process(t_main_arg *main_arg, int arg_idx, int file_fd[2])
 	// TODO: heredocにも対応させる
 	if (pipe(pipe_fd) == FAIL)
 		perror_with_exit("pipe");
-	if (arg_idx == 2)
+	if (is_first_process)
 	{
 		use_fd[0] = file_fd[0];
 		use_fd[1] = pipe_fd[1];
@@ -91,7 +96,6 @@ static void	exec_process(t_main_arg *main_arg, int arg_idx, int file_fd[2])
 		use_fd[1] = pipe_fd[1];
 	child_process(main_arg->argv[arg_idx], main_arg->envp, use_fd,
 			pipe_fd[PIPE_IN_FD]);
-	// なぜか必要
 	close(use_fd[0]);
 	close(use_fd[1]);
 }
